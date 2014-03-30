e0b2a2734a80cb22bc424b68f3e44c0a8c708243
manhinli <manhinli@gmail.com>
2014-03-30 11:29:54 +1100

    Registration form
    * Syntax fixes
    * Alterations to date range
    * Redirect for logged in users

--- scichal/scichal_user/views.py
+++ scichal/scichal_user/views.py
@@ -11,20 +11,23 @@ from django.utils import timezone
 from scichal_cms.models import Component
 
 def register(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/accounts/profile/')
+    
     menu_items = Component.objects.filter(enabled=True, menu_order__gt=0)
     
-    time_now = timezone.now
+    time_now = timezone.now()
     time_now_year = time_now.year
     
-    dob_year_select_range = range((time_now_year-120),time_now_year+1)
-    dob_year_select_default = time_now_year-12
+    dob_year_select_range = range((time_now_year-100),time_now_year+1)
+    dob_year_select_default = time_now_year-10
     
     template = 'register.html'
     
     try:
         return render(request, template, {
             'title': 'Register',
-            'show_title': true,
+            'show_title': True,
             'menu_items': menu_items,
             'dob_year_select_range': dob_year_select_range,
             'dob_year_select_default': dob_year_select_default,
